b855c6155d67b646bfe4ae2287b00c844f54357f
Cephalophile <109045174+cephalophile@users.noreply.github.com>
2023-04-26 15:24:32 -0400

    extra

--- src/ForecastSection.js
+++ src/ForecastSection.js
@@ -1,12 +1,35 @@
 import "./styles.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import ForecastDay from "./ForecastDay.js";
+import axios from "axios";
+import React,  {useState} from "react";
+
 
 export default function ForecastSection(props) {
-  console.log(props.weatherData.lat, props.weatherData.lon);
-  return (
-    <div className="ForecastSection">
-      <ForecastDay weatherData={props.weatherData} units={props.units}/>
-    </div>
-  );
+
+  const [loaded, setLoaded] = useState(false);
+  const [forecastData, setForecastData] = useState(null);
+
+  function handleResponse(response){
+    console.log(response);
+    setForecastData(response.data.daily[0]);
+    setLoaded(true);
+}
+
+  if(loaded){
+    return(
+      <div className="ForecastSection">
+        <ForecastDay forecastData={forecastData} units={props.units}/>
+      </div>
+    );  
+    } else {
+      console.log(props.weatherData);
+      let lat = props.weatherData.lat;
+      let lon = props.weatherData.lon;
+      let apiKey = `b95f179627c8dd37f41e1be6e3250e19`;
+      let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&cnt=5&appid=${apiKey}&units=metric`;
+      console.log(apiUrl);
+      axios.get(apiUrl).then(handleResponse);
+      return null;
+    }
 }
