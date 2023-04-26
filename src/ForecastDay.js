b7accdbb170230823bd34c12c434e399daa28624
Cephalophile <109045174+cephalophile@users.noreply.github.com>
2023-04-26 13:48:17 -0400

    set use state for Loaded on forecast component

--- src/ForecastDay.js
+++ src/ForecastDay.js
@@ -1,16 +1,19 @@
-import React from "react";
+import React,  {useState} from "react";
 import "./styles.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import axios from "axios";
 
 export default function ForecastDay (props){
-
-    if(props.weatherData.ready){
+    const [loaded, setLoaded] = useState(false);
+    function handleResponse(response){
+        console.log(response);
+    }
+    if(loaded){
         let lat = props.weatherData.lat;
         let lon = props.weatherData.lon;
         let apiKey = `b95f179627c8dd37f41e1be6e3250e19`;
         let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&cnt=5&appid=${apiKey}&units=metric`;
-        axios.get(apiUrl).then((response) => {showForecast(response); showForecast(); });
+        axios.get(apiUrl).then(handleResponse);
     
         function showForecast(response){
             return(
