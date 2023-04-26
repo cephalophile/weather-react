3c724b78985a5cd0a40e8ab889f16d31fbc8a8c0
Cephalophile <109045174+cephalophile@users.noreply.github.com>
2023-04-26 10:28:44 -0400

    set up ForecastDay API call

--- src/ForecastDay.js
+++ src/ForecastDay.js
@@ -5,34 +5,36 @@ import axios from "axios";
 
 export default function ForecastDay (props){
 
-    let lat = props.weatherData.lat;
-    let lon = props.weatherData.lon;
-    let apiKey = `b95f179627c8dd37f41e1be6e3250e19`;
-    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&cnt=5&appid=${apiKey}&units=metric`;
-    axios.get(apiUrl).then(showForecast);
-
-    function showForecast(response){
-    return(
-        <div className="forecastArea day1">
-        <p className="weatherIcon" id="day1-icon">
-          ☀
-        </p>
-        <p className="forecastDay" id="day1-day">
-          Thu
-        </p>
-        <p className="forecastDate" id="day1-date">
-          Aug 12
-        </p>
-        <p>
-          <span className="forecastHigh temperature" id="day1-high">
-            82°
-          </span>{" "}
-          |{" "}
-          <span className="forecastLow temperature" id="day1-low">
-            66°
-          </span>
-        </p>
-      </div>
-    );
+    if(props.weatherData.ready){
+        let lat = props.weatherData.lat;
+        let lon = props.weatherData.lon;
+        let apiKey = `b95f179627c8dd37f41e1be6e3250e19`;
+        let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&cnt=5&appid=${apiKey}&units=metric`;
+        axios.get(apiUrl).then((response) => {showForecast(response); showForecast(); });
+    
+        function showForecast(response){
+            return(
+                <div className="forecastArea day1">
+                <p className="weatherIcon" id="day1-icon">
+                ☀
+                </p>
+                <p className="forecastDay" id="day1-day">
+                Thu
+                </p>
+                <p className="forecastDate" id="day1-date">
+                Aug 12
+                </p>
+                <p>
+                <span className="forecastHigh temperature" id="day1-high">
+                    82°
+                </span>{" "}
+                |{" "}
+                <span className="forecastLow temperature" id="day1-low">
+                    66°
+                </span>
+                </p>
+            </div>
+            );
+        }
     }
 }
